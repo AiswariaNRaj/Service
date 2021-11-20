1c19d6ae10614ecea50d1db4e0beca6c7675bc81
AiswariaNRaj <aiswarianraj1996@gmail.com>
2021-11-20 22:13:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,45 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.Toast;
+
+import SepratePackage.aidlInterface;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+
+
+    aidlInterface.Stub stub = new aidlInterface.Stub() {
+    @Override
+    public int PerformPreviousPlay() throws RemoteException {
+
+        return Log.d("PreviousSong","Previous Song Played");
+
+
+    }
+
+    @Override
+    public int PerformCurrentPlay() throws RemoteException {
+
+        return   Log.d("CurrentSong","Current Song Played");
+
+    }
+
+    @Override
+    public int PerformNextPlay() throws RemoteException {
+
+        return Log.d("NextSong","Next Song Played");
+    }
+};
+    @Override
+    public IBinder onBind(Intent intent) {
+       return stub;
+    }
+
+}
