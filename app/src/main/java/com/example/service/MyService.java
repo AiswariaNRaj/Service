1c96c4d96acbd3b79cd5d7adca358f4d6b2f9f81
AiswariaNRaj <aiswarianraj1996@gmail.com>
2021-11-23 18:54:42 +0530

    initial commit

--- app/src/main/java/com/example/service/MyService.java
+++ app/src/main/java/com/example/service/MyService.java
@@ -2,34 +2,50 @@ package com.example.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
-import android.widget.Toast;
 
 import SepratePackage.aidlInterface;
 
-public class MyService extends Service {
+public class MyService extends Service implements MediaPlayer.OnPreparedListener {
     public MyService() {
     }
 
 
-
+MediaPlayer mediaPlayer = null;
     aidlInterface.Stub stub = new aidlInterface.Stub() {
-    @Override
-    public int PerformPreviousPlay() throws RemoteException {
-
-        return Log.d("PreviousSong","Previous Song Played");
 
 
-    }
+        @Override
+        public int PerformPreviousPlay() throws RemoteException {
+            return Log.d("PreviousSong","Previous Song Played");
 
-    @Override
+        }
+
+        @Override
     public int PerformCurrentPlay() throws RemoteException {
-
-        return   Log.d("CurrentSong","Current Song Played");
-
-    }
+            mediaPlayer = new MediaPlayer();
+//      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//      if(mediaPlayer.isPlaying()){
+//          mediaPlayer.start();
+//        Log.d("CurrentSong","Current Song Played");}
+//      else {
+//          mediaPlayer.pause();
+//            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//            if (mediaPlayer.isPlaying()){
+//                mediaPlayer.start();
+//                Log.d("CurrentSong","Current Song Played");
+//            }else {}
+              return   Log.d("CurrentSong","Current Song Played");
+////        Intent intent
+////        if ()
+//mediaPlayer.pause();
+       //  return    Log.d("PauseSong","Current Song Paused");
+            //return 0;
+        }
 
     @Override
     public int PerformNextPlay() throws RemoteException {
@@ -41,7 +57,13 @@ public class MyService extends Service {
     };
     @Override
     public IBinder onBind(Intent intent) {
+
+
        return stub;
     }
 
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
 }
