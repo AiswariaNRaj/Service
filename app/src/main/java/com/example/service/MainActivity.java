bf804289b1f1c6e330f704235815879262f46853
Aiswaria N Raj <aiswarianraj1996@gmail.com>
2022-01-12 11:45:32 +0530

    initial commit

--- app/src/main/java/com/example/service/MainActivity.java
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -6,13 +6,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-
 import android.content.Intent;
 import android.content.pm.PackageManager;
-
 import android.os.Build;
 import android.os.Bundle;
-
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE = 1;
@@ -21,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,MyService.class);
+        Intent intent = new Intent(this,MediaService.class);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             startForegroundService(intent);
         }else{
@@ -29,21 +27,19 @@ public class MainActivity extends AppCompatActivity {
         }
         permission();
     }
-
     private void permission() {
         if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(MainActivity.this,new String[]{
                     Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
         }
-
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode==REQUEST_CODE){
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
             }
             else{
                 ActivityCompat.requestPermissions(MainActivity.this,new String[]{
